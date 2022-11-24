4b7995789c7e3234d18784d14849ab40bfeef991
pisoprintsmr <118580297+pisoprintsmr@users.noreply.github.com>
2022-11-24 13:18:42 +0800

    Delete Inventory directory

--- Inventory/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-   $dbServer = "localhost";
-   $dbUser = "root";
-   $dbPass = "";
-   $dbtable = "breadjoint"; 
-   $connect = mysqli_connect($dbServer, $dbUser, $dbPass, $dbtable);
-   
-?>
